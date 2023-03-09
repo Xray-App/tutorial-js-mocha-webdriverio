30288f951cbaa3861f0045bfc1781d74cab6f685
CMCunha <cristiano.cunha@gmail.com>
2023-03-09 11:41:18 +0000

    Updating Xray endpoint.

--- import-results-cloud.sh
+++ import-results-cloud.sh
@@ -1,5 +1,5 @@
 # submit from the command line
-BASE_URL=https://xray.cloud.xpand-it.com
+BASE_URL=https://xray.cloud.getxray.app
 token=$(curl -H "Content-Type: application/json" -X POST --data @"cloud_auth.json" "$BASE_URL/api/v1/authenticate"| tr -d '"')
 curl -H "Content-Type: application/xml" -X POST -H "Authorization: Bearer $token"  --data @"results.xml" "$BASE_URL/api/v2/import/execution/junit?projectKey=COM&testPlanKey=COM-5"
 
